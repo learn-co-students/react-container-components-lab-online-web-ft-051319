e5bbda093e5e025c3c8d5d0d456abcb655789c7f
Kane Koo <kanekoo@gmail.com>
2019-10-12 23:17:17 -0400

    Done.

--- src/components/LatestMovieReviewsContainer.js
+++ src/components/LatestMovieReviewsContainer.js
@@ -7,3 +7,34 @@ const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/all.json?'
             + `api-key=${NYT_API_KEY}`;
 
 // Code LatestMovieReviewsContainer Here
+
+class LatestMovieReviewsContainer extends Component {
+  constructor() {
+    super()
+
+    this.state = {
+      reviews:[]
+    }
+  }
+
+  componentDidMount() {
+    this.fetchReviews()
+  }
+  fetchReviews(){
+    fetch(URL)
+      .then (resp => resp.json())
+      .then (resp => this.setState({reviews: resp.results}))
+        //this.setState({reviews: resp.data}))
+
+  }
+  render() {
+    return(
+
+      <div className="latest-movie-reviews">
+      <MovieReviews className="review-list" reviews={this.state.reviews} />
+      </div>
+    )
+  }
+}
+
+export default LatestMovieReviewsContainer
--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -1,1 +1,42 @@
 // Code MovieReviews Here
+import React from 'react'
+
+function MovieReviews(props) {
+  return (
+      props.reviews.map((review, idx) => {
+       return (<div className="review" id={idx}>Title: {review.display_title}; Short: {review.summary_short} </div>)
+        })
+
+
+     )
+    }
+
+
+export default MovieReviews
+/*
+ const mapMovieReviews = (props) => {
+      props.reviews.map((review, idx) => {
+       return (<div id={idx}>Title: {review.display_title}; Short: {review.summary_short} </div>)
+          }
+        )
+      }
+
+const MovieReviews = (props) => {
+  return (
+//  I tried to put a <div className = "review-list"> here but I kept getting an error
+    <div>
+      { (props) => { return
+           (props.reviews.map((review, idx) => {
+            return (<div id={idx}>Title: {review.display_title}; Short: {review.summary_short} </div>)
+               }
+             )
+           )
+
+
+         }
+       }
+      </div>
+    )
+
+}
+*/
